f13ba98c4c34a1eb6e85a684c4681058fe9cf1e8
okadareo <okadareo0614@gmail.com>
2020-12-14 17:26:45 +0000

    [Start] 7-0.1.2.3.4

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e2794d919d61a159b495c6a35ce4b82312734129e779fc650414c895b96f6b9caa252b30102272decf0e80271eb95fdaf8fe975ce163a9037229233f654a6003'
+Refile.secret_key = 'a6dcad014492d1fc8395ab178f12664c13c5f1a9bf669737dcea7a8c7ef9530759d257362d0fedb2dc3dd878bf8b8544f68b405059c7ba6833adaa2adef65745'
